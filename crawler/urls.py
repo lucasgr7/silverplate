2ed8d0eaa939f76cc7bcd382a3bbe910a0b43b32
lucasgr7 <lucasgr7@gmail.com>
2016-09-21 23:25:49 -0300

    Task: Create RUN page to crawler
    Created a page to run the proccess of the crawler, leaving the necessity
    to run on console the init.py
    Now it's left data mining and feedback on the proccess

--- crawler/urls.py
+++ crawler/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^salvar_palavra_ignorar$', views.salvar_palavra_ignorar, name='salvar_palavra_ignorar'),
     url(r'^salvar_ingrediente$', views.salvar_Ingrediente, name='salvar_ingrediente'),
     url(r'^delete_spec$', views.delete_spec, name='salvar_ingrediente'),
+    url(r'^run_crawler$', views.run_crawler, name='run_crawler'),
 ]
--- crawler/views.py
+++ crawler/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponseRedirect
 from django.views.generic import ListView
 from django.shortcuts import render
 
+import urllib.request
+
 from .models import IngredientSpec, IgnoredWords
 from objetos.models import Ingredient
+from crawler.engine import LinkFinder
+from crawler.engine import IngredientFinder
 
 
 # Create your views here.
@@ -72,3 +76,49 @@ def clear_specs(new_ingredient):
             spec.delete()
     except IngredientSpec.DoesNotExist:
         print('sem chance')
+
+def run_crawler(request):
+    message = 'Finished the proccess'
+    if request.method == 'POST':
+        link = request.POST.get('url')
+        number_access = request.POST.get('number_access')
+        print('link : ' + link)
+        if not number_access or number_access == None or number_access == '':
+            message = 'You must inform number of access'
+        if not link or number_access == None:
+            message = 'You must inform the link to start crawling'
+        if len(link) > 0:    
+            try:
+                number_access = int(number_access)
+                response = urllib.request.urlopen(link)
+                html = response.read().decode('utf-8')
+                parser = LinkFinder()
+                parser.feed(html)
+                i = 0
+                while number_access > 0:
+                    link = parser.links[i]
+                    print(link)
+                    print('number_access ' + str(number_access))
+                    response = urllib.request.urlopen(link)
+                    html = response.read().decode('utf-8')
+                    parser.feed(html)
+                    i += 1
+                    number_access -= 1
+
+                print('links encontrados : ' + str(len(parser.links)))
+                print('Will start recovering data from the site')
+
+                DataParser = IngredientFinder()
+                size = len(parser.links)
+                for link in parser.links:
+                    size -= 1
+                    response = urllib.request.urlopen(link)
+                    html = response.read().decode('utf-8')
+                    DataParser.feed(html)
+
+                print('Found %s ingredients' % DataParser.ingredientes)
+                print('Found %s Steps Cooking' % DataParser.passos)
+            except Exception as e:
+                message = str(e)            
+
+    return render(request, 'crawler/home.html', {'message':message})
